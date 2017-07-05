ced3510afc2b7cbdf4ee74697e7f0060dcb104a2
Wei Wang <yuangwang2@gmail.com>
2017-07-05 18:17:26 -0500

    scraper works for one page

--- scraper.py
+++ scraper.py
@@ -8,61 +8,121 @@ from bs4 import BeautifulSoup
 # username: br26346
 # password: example
 
+# fun fact UT has 402 pages in their course directory
 
-def scrape_catalog():
+def scrape_catalog(session):
+    # TODO finish this function, right now just scrapes first page
+    start_page = "https://utdirect.utexas.edu/apps/registrar/course_schedule/20179/results/?ccyys=20179&" \
+                 "search_type_main=UNIQUE&unique_number=&start_unique=00000&end_unique=99999"
+    scrape_page(session, start_page)
 
-    # main_page = requests.get("https://utdirect.utexas.edu/apps/registrar/course_schedule/20179/")
-    main_page = requests.get("http://www.pythonforbeginners.com")
-    c = main_page.content
-    soup = BeautifulSoup(c)
-    for link in soup.find_all('a'):
-        print(link.get("href"))
+
+# search directory for unique numbers 00000 to 99999
+def scrape_page(session, page):
+    # full catalog in main_page
+    main_page = session.get(page)
+    soup = BeautifulSoup(main_page.content, "lxml")
+
+    # go through each row of the catalog
+    for row in soup.find_all("tr"):
+
+        # unique check only allows the class to be stored once its data is compeltely scraped
+        unique_check = False
+
+        for col in row.find_all("td"):
+            # finds the class name for following courses
+            try:
+                if col["class"][0] == "course_header":
+                    name = col.get_text()
+
+            # finds and stores the rest of the course info
+            except KeyError:
+                try:
+                    if col["data-th"] == "Unique":
+                        unique = col.get_text()
+                        unique_check = True
+                    if col["data-th"] == "Days":
+                        day = col.get_text()
+                    if col["data-th"] == "Hour":
+                        hour = col.get_text()
+                    if col["data-th"] == "Room":
+                        room = col.get_text()
+                    if col["data-th"] == "Instructor":
+                        professor = col.get_text()
+                    if col["data-th"] == "Status":
+                        status = col.get_text()
+                except KeyError:
+                    continue
+
+        # called when a single class has all of its info and stores its info
+        if unique_check is True:
+            course = CourseInfo(unique, day, hour, room, professor, status, name)
+            course.store()
+
+
+# contains the info for the course
+class CourseInfo:
+    def __init__(self, unique, day, hour, room, professor, status, name):
+        self.unique = unique
+        self.day = day
+        self.hour = hour
+        self.room = room
+        self.professor = professor
+        self.status = status
+        self.name = name
+
+    # stores data into SQL database
+    def store(self):
+        # TODO finish this method to store CourseInfo into a SQL database
+        return
 
 
 # logs into course catalog
+# takes in the log in info for the scraper account
+# returns the session that is already logged in
 def login(username, password):
-    # injects login info and redirect info to navigate to the base course page
 
     # for login post form
     login_info = [
-        ('IDToken1', username),
-        ('IDToken2', password),
-        ('login_uri', '/login/cdcservlet'),
-        ('login_method', 'GET'),
-        ('login_param_MinorVersion', '0'),
-        ('login_param_RequestID', '1498963635061'),
-        ('login_param_ProviderID', 'https://utdirect.utexas.edu:443/amagent?Realm=/admin/utdirect-realm'),
-        ('login_param_MajorVersion', '1'),
-        ('IDButton', 'Log In'),
-        ('goto', 'https://utdirect.utexas.edu:443/apps/registrar/course_schedule/20179'),
-        ('SunQueryParamsString',
-         'TWlub3JWZXJzaW9uPTAmUmVxdWVzdElEPTE0OTg5NjM2MzUwNjEmUHJvdmlkZXJJRD1odHRwczovL3V0ZGlyZWN0LnV0ZXhhcy5lZHU6NDQzL2FtYWdlbnQ/UmVhbG09L2FkbWluL3V0ZGlyZWN0LXJlYWxtJklzc3VlSW5zdGFudD0yMDE3LTA3LTAyVDAyOjQ3OjE1WiZNYWpvclZlcnNpb249MQ=='),
-        ('encoded', 'false'),
-        ('gx_charset', 'UTF-8'),
+        ("IDToken1", username),
+        ("IDToken2", password),
+        ("login_uri", "/login/cdcservlet"),
+        ("login_method", "GET"),
+        ("login_param_MinorVersion", "0"),
+        ("login_param_RequestID", "1498963635061"),
+        ("login_param_ProviderID", "https://utdirect.utexas.edu:443/amagent?Realm=/admin/utdirect-realm"),
+        ("login_param_MajorVersion", "1"),
+        ("IDButton", "Log In"),
+        ("goto", "https://utdirect.utexas.edu:443/apps/registrar/course_schedule/20179"),
+        ("SunQueryParamsString",
+         "TWlub3JWZXJzaW9uPTAmUmVxdWVzdElEPTE0OTg5NjM2MzUwNjEmUHJvdmlkZXJJRD1odHRwczovL3V0ZGlyZWN0L"
+         "nV0ZXhhcy5lZHU6NDQzL2FtYWdlbnQ/UmVhbG09L2FkbWluL3V0ZGlyZWN0LXJlYWxtJklzc3VlSW5zdGFudD0yMD"
+         "E3LTA3LTAyVDAyOjQ3OjE1WiZNYWpvclZlcnNpb249MQ=="),
+        ("encoded", "false"),
+        ("gx_charset", "UTF-8"),
     ]
 
     # remember session to stay logged in, store cookies and the such
     scrape_session = requests.session()
 
     # submit the login info
-    scrape_session.post('https://login.utexas.edu/login/UI/Login', data=login_info)
-    time.sleep(2)
+    scrape_session.post("https://login.utexas.edu/login/UI/Login", data=login_info)
+    time.sleep(1)
 
     # page gets redirected, need to input LARES value to continue, no clue what LARES is
-    lares_page = scrape_session.get('https://utdirect.utexas.edu/apps/registrar/course_schedule/20179')
-    time.sleep(2)
+    lares_page = scrape_session.get("https://utdirect.utexas.edu/apps/registrar/course_schedule/20179")
+    time.sleep(1)
 
     # goes through the page and finds the LARES value to input
     lares_soup = BeautifulSoup(lares_page.content, "lxml")
-    lares = lares_soup.find('input', {'name': 'LARES'})['value']
+    lares = lares_soup.find("input", {"name": "LARES"})["value"]
     lares_data = [
-        ('LARES', lares)
+        ("LARES", lares)
     ]
 
     # submits the LARES value, this time going into the correct path
-    scrape_session.post('https://utdirect.utexas.edu/apps/registrar/course_schedule/20179', data=lares_data)
-    time.sleep(2)
+    scrape_session.post("https://utdirect.utexas.edu/apps/registrar/course_schedule/20179", data=lares_data)
+    time.sleep(1)
 
     # returns the session for future use
     return scrape_session
-
