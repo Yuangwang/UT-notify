7ddff173afbb97bfe107343bcb45696c0136070c
Wei Wang <yuangwang2@gmail.com>
2017-07-05 18:17:26 -0500

    scraper works for one page

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import scraper
 
+
 def main():
     # logs into scraper dummy account
     scrape_session = scraper.login("br26346", "29900770ben")
-
+    scraper.scrape_catalog(scrape_session)
 
 
 if __name__ == "__main__":
