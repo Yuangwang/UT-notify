e79b8f2b417ca729cf14347e750bc688d0ff0b52
Wei Wang <yuangwang2@gmail.com>
2017-07-13 22:28:27 -0500

    notifications database interactions done

--- Scraping script/main.py
+++ Scraping script/main.py
@@ -1,11 +1,12 @@
 import scraper
-import psycopg2
+import notifications
 
 
 def main():
     # logs into scraper dummy account
-    scrape_session = scraper.login("br26346", "29900770ben")
-    scraper.scrape_catalog(scrape_session)
+    # scrape_session = scraper.login("br26346", "29900770ben")
+    # scraper.scrape_catalog(scrape_session)
+    notifications.notify_user()
 
 
 
--- Scraping script/notifications.py
+++ Scraping script/notifications.py
@@ -1,3 +1,19 @@
-import data
+import psycopg2
 
+
+def notify_user():
+    # opens connection and cursor for future use
+    con = psycopg2.connect("dbname='myBot' user='postgres' host='localhost' password='29900770wei'")
+    cur = con.cursor()
+
+    # pending_notifs is a list of duples in the format ('phonenumber', uniquenumber)
+    cur.execute("SELECT * FROM userinfo")
+    pending_notifs = cur.fetchall()
+
+    for notif in pending_notifs:
+        cur.execute("SELECT name FROM course WHERE id =%s", (notif[1], ))
+        print(cur.fetchall())
+
+    con.close()
+    cur.close()
 
