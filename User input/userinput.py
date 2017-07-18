c0b3089905077b1c490c4f0ebacf78e2fa694dea
Wei Wang <yuangwang2@gmail.com>
2017-07-18 02:24:42 -0500

    notifications started and mostly done, needs to be polished

--- User input/userinput.py
+++ User input/userinput.py
@@ -7,14 +7,17 @@ def main():
     con = psycopg2.connect("dbname='myBot' user='postgres' host='localhost' password='29900770wei'")
     cur = con.cursor()
 
-    phone = input("Phone Number in the format +xxxxxxxxxx:")
+    phone = input("Phone Number in the format +1xxxxxxxxxx:")
 
     classes = input("How many classes would you like to watch?")
     courses = int(classes)
 
     for course in range(courses):
         unique = input("Unique Number:")
-        cur.execute("INSERT INTO userinfo (phone, classunique) values(%s, %s)", (phone, unique))
+        status = input("Watch for open or waitlisted?")
+        cur.execute("INSERT INTO userinfo (phone, classunique, status) values(%s, %s, %s)", (phone, unique, status))
+        status += "; restricted"
+        cur.execute("INSERT INTO userinfo (phone, classunique, status) values(%s, %s, %s)", (phone, unique, status))
 
     con.commit()
     con.close()
