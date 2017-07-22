8a4db3c81c9e674a32536c717e655f2bee19f542
Wei Wang <yuangwang2@gmail.com>
2017-07-21 22:05:29 -0500

    actually done except auto registration

--- Scraping script/main.py
+++ Scraping script/main.py
@@ -4,8 +4,8 @@ import notifications
 
 def main():
     # logs into scraper dummy account
-    # scrape_session = scraper.login("br26346", "example")
-    # scraper.scrape_catalog(scrape_session)
+    scrape_session = scraper.login("br26346", "example")
+    scraper.scrape_catalog(scrape_session)
     notifications.notify_user()
 
 
