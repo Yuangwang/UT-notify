80dd100c5ae30259e07d2781487c5479cdbb5158
Wei Wang <yuangwang2@gmail.com>
2018-04-11 19:32:10 -0600

    changes added for new semester

--- Scraping script/scraper.py
+++ Scraping script/scraper.py
@@ -28,7 +28,7 @@ def scrape_catalog(session):
     online_uniques = []
 
     # root page to be scraped
-    next_page = "https://utdirect.utexas.edu/apps/registrar/course_schedule/20179/results/?ccyys=20179&" \
+    next_page = "https://utdirect.utexas.edu/apps/registrar/course_schedule/20189/results/?ccyys=20189&" \
                 "search_type_main=UNIQUE&unique_number=&start_unique=00000&end_unique=99999"
 
     # tests to see if the end has been reached, if not keeps scraping
@@ -94,7 +94,7 @@ def scrape_page(session, page, db_uniques, on_uniques, cur, con):
     # look for the next page to crawl
     try:
         test = page_soup.find("a", {"id": "next_nav_link"})["href"]
-        return "https://utdirect.utexas.edu/apps/registrar/course_schedule/20179/results/" + test
+        return "https://utdirect.utexas.edu/apps/registrar/course_schedule/20189/results/" + test
     except TypeError:
         return None
 
@@ -132,16 +132,8 @@ def login(username, password):
         ("IDToken2", password),
         ("login_uri", "/login/cdcservlet"),
         ("login_method", "GET"),
-        ("login_param_MinorVersion", "0"),
-        ("login_param_RequestID", "1498963635061"),
-        ("login_param_ProviderID", "https://utdirect.utexas.edu:443/amagent?Realm=/admin/utdirect-realm"),
-        ("login_param_MajorVersion", "1"),
         ("IDButton", "Log In"),
-        ("goto", "https://utdirect.utexas.edu:443/apps/registrar/course_schedule/20179"),
-        ("SunQueryParamsString",
-         "TWlub3JWZXJzaW9uPTAmUmVxdWVzdElEPTE0OTg5NjM2MzUwNjEmUHJvdmlkZXJJRD1odHRwczovL3V0ZGlyZWN0L"
-         "nV0ZXhhcy5lZHU6NDQzL2FtYWdlbnQ/UmVhbG09L2FkbWluL3V0ZGlyZWN0LXJlYWxtJklzc3VlSW5zdGFudD0yMD"
-         "E3LTA3LTAyVDAyOjQ3OjE1WiZNYWpvclZlcnNpb249MQ=="),
+        ("goto", "https://utdirect.utexas.edu:443/apps/registrar/course_schedule/20189"),
         ("encoded", "false"),
         ("gx_charset", "UTF-8"),
     ]
@@ -154,7 +146,7 @@ def login(username, password):
     time.sleep(1)
 
     # page gets redirected, need to input LARES value to continue, no clue what LARES is
-    lares_page = scrape_session.get("https://utdirect.utexas.edu/apps/registrar/course_schedule/20179")
+    lares_page = scrape_session.get("https://utdirect.utexas.edu/apps/registrar/course_schedule/20189")
     time.sleep(1)
 
     # goes through the page and finds the LARES value to input
@@ -165,7 +157,7 @@ def login(username, password):
     ]
 
     # submits the LARES value, this time going into the correct path
-    scrape_session.post("https://utdirect.utexas.edu/apps/registrar/course_schedule/20179", data=lares_data)
+    scrape_session.post("https://utdirect.utexas.edu/apps/registrar/course_schedule/20189", data=lares_data)
     time.sleep(1)
 
     # returns the session for future use
